a17d7b264d674811b95f612e6278ead959e0df49
aboul12344 <107145708+aboul12344@users.noreply.github.com>
2022-06-08 20:07:02 -0300

    myprojectspython
    eu estou adicionada um novo linha 4

---
+++ myprojectpython/text.py
@@ -0,0 +1,17 @@
+   print('hello world')
+   print('linha 2')
+   print('rachid')
+   print('nova linha')
+   }
+   valor=10
+   if valor<=10
+   
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+aboul12344/aboul12344 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
